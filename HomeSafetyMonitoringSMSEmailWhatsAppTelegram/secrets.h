567083da3580b02fb9f7ab97f532cab7ac0be12f
Natalie <118202686+Nokuvimba@users.noreply.github.com>
2024-05-03 11:52:23 +0100

    Delete HomeSafetyMonitoringSMSEmailWhatsAppTelegram directory

--- HomeSafetyMonitoringSMSEmailWhatsAppTelegram/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "B535_B23D"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "TgdN3d4BGYb"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 2412150			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "TPCO2IHJINGH3880"   // replace XYZ with your channel write API Key
